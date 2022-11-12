ab34a59ef8974849845162d9bb5a00e3c13d5d69
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-11-12 12:19:15 +0600

    Improve the `ResultPopup` component

--- components/ResultPopup.tsx
+++ components/ResultPopup.tsx
@@ -1,23 +1,43 @@
+import { USER_VALUE } from "../pages";
+
 type ResultPopupProps = {
     winner: string | number;
 };
 
+// Popup component that shows the winner
 export default function ResultPopup({ winner }: ResultPopupProps) {
+    // if winner is a number, it means draw. Nobody won
     if (winner == 0) {
         return (
-            <div>
-                <h2>Draw</h2>
-                <button onClick={() => window.location.reload()}>
+            <div className="result-popup draw">
+                <h2 className="text">Game Draw</h2>
+                <button
+                    className="play-button restart"
+                    onClick={() => window.location.reload()}
+                >
                     Play again
                 </button>
             </div>
         );
     }
 
+    // if winner is a string, it means someone won
+    // either user or computer
     return (
-        <div>
-            <h2>{winner} won!</h2>
-            <button onClick={() => window.location.reload()}>Play again</button>
+        <div
+            className={`result-popup ${
+                winner === USER_VALUE ? "user" : "computer"
+            }`}
+        >
+            <h2 className="text">
+                {winner === USER_VALUE ? "You" : "Computer"} won!
+            </h2>
+            <button
+                className="play-button restart"
+                onClick={() => window.location.reload()}
+            >
+                Play again
+            </button>
         </div>
     );
 }
