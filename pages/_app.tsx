316235e2ca6c5c32771e36cf966dab4942dd7237
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-11-13 15:20:35 +0600

    Include vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Navbar />
             <Component {...pageProps} />
+            <Analytics />
         </>
     );
 }
